5da0960f109e8f768d604f5ac4942222451c0985
Steven Chang <s8110282004@gmail.com>
2018-12-14 13:33:40 +0800

    version update to v1.0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'UserAgentPool',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'Use for randomly get header',
     author = 'Steven Chang',
     author_email = 's8110282004@gmail.com',
     url = 'https://github.com/MuFanChang/UserAgentPool',
-    download_url = 'https://github.com/MuFanChang/UserAgentPool/releases/tag/v1.0.2',
-    scripts = 'UserAgent.py',
+    download_url = 'https://github.com/MuFanChang/UserAgentPool/releases/tag/v1.0.4',
     packages = find_packages(),
     keywords = ['UserAgent','header'],
     classifiers = []
